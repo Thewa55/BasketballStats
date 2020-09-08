0d73f0cafce68d1f167a970241033413b9769b20
Kenny Lam <kinwai.lam730@gmail.com>
2020-09-08 15:23:19 -0400

    Added an ajax call

--- assets/js/players.js
+++ assets/js/players.js
@@ -4,8 +4,25 @@ $("#playerSearch").on("click", function(event){
     event.preventDefault();
     playerName = $(".form-control").val();
     console.log(playerName)
-    if(playerName != ""){
-        return console.log("Player searched!")
+    if(playerName === ""){
+        return console.log("Search is empty!")
     }
-
+    console.log(playerName + " After if")
 })
+
+function initialize(){
+    let URL = "https://www.balldontlie.io/api/v1/players"
+    console.log(URL)
+    $.ajax({
+        url: URL,
+        method: "GET",
+        statusCode: {404: ()=> {
+            console.log("Can't fetch API");
+            return
+        }}
+    }).then(function(response){
+        console.log(response);
+    })
+}
+
+initialize();
