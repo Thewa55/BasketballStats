5eaffe321b6d7dd0e8c612e6f822af456ea52e8f
Kenny Lam <kinwai.lam730@gmail.com>
2020-09-08 21:47:57 -0400

    Added an ajax call for a player

--- assets/js/players.js
+++ assets/js/players.js
@@ -7,11 +7,15 @@ $("#playerSearch").on("click", function(event){
     if(playerName === ""){
         return console.log("Search is empty!")
     }
-    console.log(playerName + " After if")
     playerSearch(playerName)
 })
 
-function playerSearch(playerName){
+let initialize = () => {
+  $("#playerSection").hide();
+}
+
+
+let playerSearch = (playerName) => {
     let URL = "https://www.balldontlie.io/api/v1/players?search="+playerName+"&per_page=100"
     console.log(URL)
     $.ajax({
@@ -22,6 +26,32 @@ function playerSearch(playerName){
             return
         }}
     }).then(function(response){
+        if(response.data.length === 1){
+            $("#playerSection").show();
+            console.log(response.data)
+            let playerData = response.data[0]
+            displayData(response.data[0].id)
+            let playerNameHeader = $("<h2>").text(playerData.first_name + " " + playerData.last_name);
+            let playerPosition = $("<p>").text(playerData.position + " | " + playerData.team.abbreviation)
+            $("#playerName").append(playerNameHeader);
+            $("#playerName").append(playerPosition);
+        }
+    })
+}
+
+let displayData = (playerId) => {
+    console.log(playerId)
+    let playerURL = "https://www.balldontlie.io/api/v1/season_averages?player_ids[]="+playerId
+    $.ajax({
+        url: playerURL,
+        method: "GET",
+        statusCode: {404: ()=> {
+            console.log("Can't fetch API");
+            return;
+        }}
+    }).then(function(response){
         console.log(response);
     })
 }
+
+initialize();
